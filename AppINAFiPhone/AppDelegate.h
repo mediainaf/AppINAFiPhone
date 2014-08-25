e5f82dca77ffb3c80f1c47eebe1cb57416765cfb
Nicolò Parmiggiani <nicolo.parmiggiani@gmail.com>
2014-08-25 12:19:31 +0200

    License

--- AppINAFiPhone/AppDelegate.h
+++ AppINAFiPhone/AppDelegate.h
@@ -3,8 +3,10 @@
 //  AppINAFiPhone
 //
 //  Created by Nicolo' Parmiggiani on 25/08/14.
-//  Copyright (c) 2014 Nicolo' Parmiggiani. All rights reserved.
-//
+//  Created by Nicolo' Parmiggiani on 12/02/14.
+// Copyright (c) 2014 Nicolò Parmiggiani. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #import <UIKit/UIKit.h>
 

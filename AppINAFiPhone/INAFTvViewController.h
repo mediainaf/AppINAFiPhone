033b7c8d1bfb69d5743f59f6d6fb66543d6431a3
Nicolò Parmiggiani <nicolo.parmiggiani@gmail.com>
2015-09-04 18:39:33 +0200

    Minor fix

--- AppINAFiPhone/INAFTvViewController.h
+++ AppINAFiPhone/INAFTvViewController.h
@@ -4,7 +4,8 @@
 //
 //  Created by Nicolo' Parmiggiani on 03/09/15.
 //  Copyright (c) 2015 Nicolo' Parmiggiani. All rights reserved.
-//
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 
 #import <UIKit/UIKit.h>
 
